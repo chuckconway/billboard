8f2d1427bbaa716f948c9c550ce26971c7cd869d
lostinkali <lostinkali@localhost>
2012-12-14 14:22:09 -0800

    Added Timezone data to the database project.

---
+++ Source/Billboard.Database/Post_Deploy.sql
@@ -0,0 +1,18 @@
+﻿
+
+SET IDENTITY_INSERT [dbo].[Timezone] ON 
+
+GO
+INSERT [dbo].[Timezone] ([Id], [Name], [OffSetHour], [OffSetMinutes]) VALUES (1, N'Pacific Standard Time', -8, 0)
+GO
+INSERT [dbo].[Timezone] ([Id], [Name], [OffSetHour], [OffSetMinutes]) VALUES (2, N'Alaskan Standard Time', -9, 0)
+GO
+INSERT [dbo].[Timezone] ([Id], [Name], [OffSetHour], [OffSetMinutes]) VALUES (3, N'Mountain Standard Time', -7, 0)
+GO
+INSERT [dbo].[Timezone] ([Id], [Name], [OffSetHour], [OffSetMinutes]) VALUES (4, N'Central Standard Time', -6, 0)
+GO
+INSERT [dbo].[Timezone] ([Id], [Name], [OffSetHour], [OffSetMinutes]) VALUES (5, N'Eastern Standard Time', -5, 0)
+GO
+INSERT [dbo].[Timezone] ([Id], [Name], [OffSetHour], [OffSetMinutes]) VALUES (6, N'Hawaiian Standard Time', -10, 0)
+GO
+SET IDENTITY_INSERT [dbo].[Timezone] OFF
